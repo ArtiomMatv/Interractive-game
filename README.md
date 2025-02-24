06d5201c5e7bf32ead62b83173d117ebe835eea0
Artiom377 <a.matveiciuc@student.rug.nl>
2025-02-24 17:41:46 +0100

    Local to public

--- README.md
+++
@@ -1,1 +0,0 @@
-# Interractive-game
---
+++ main.py
@@ -0,0 +1,5 @@
+from rpg.game import Game
+
+if __name__ == "__main__":
+    game_instance = Game()  # Create an instance of the Game class
+    game_instance.run()  # Start the game
